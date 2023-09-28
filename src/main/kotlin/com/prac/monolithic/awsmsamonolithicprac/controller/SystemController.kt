f0e2e7ef5168e4398951beeeb1ae88fd2cd1c4bb
burger <burger@burgerui-MacBookPro.local>
2023-09-28 12:37:43 +0900

    health_check branch명 변경

--- src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/controller/SystemController.kt
+++ src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/controller/SystemController.kt
@@ -19,7 +19,7 @@ class SystemController {
             "ipAddress" to InetAddress.getLocalHost().hostAddress,
             "timestamp" to LocalDateTime.now(),
             "cpuUsage" to getCurrentCpuUsage(),
-            "branch" to "2_monolithic_cloud-V2",
+            "branch" to "2_monolithic_cloud_v2",
         )
     )
 
